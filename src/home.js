08685c514867f5cea2ffc21f0bf4b5682898d5a3
SiamX003 <ahnaf.siam750@gmail.com>
2025-08-16 17:13:38 +0600

    first

---
+++ src/home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./home.css"; // keep your styles in the same CSS file
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <span className="logo-box">J</span>
+        <span className="logo-text">Stack</span>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Browse Jobs</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <a href="#" className="join-btn">Join</a>
+    </nav>
+  );
+};
+
+export default Navbar;
